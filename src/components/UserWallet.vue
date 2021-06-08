7729ac022d9009766a5326ae5b5bee1e86f01d7e
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-06-08 14:03:30 +0900

    Vue4-6

---
+++ src/components/UserWallet.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="overlay">
+    <div id="content">
+      <p>
+        {{ user.name }}さんの残高<br /><br />
+        {{ user.wallet }}
+      </p>
+      <div>
+        <button @click="closeWallet">close</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['user'],
+
+  methods: {
+    closeWallet() {
+      this.$emit('close-click');
+    },
+  },
+};
+</script>
+<style scoped>
+#overlay {
+  z-index: 1;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#content {
+  z-index: 2;
+  width: 25%;
+  padding: 1em;
+  background: #fff;
+}
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="users">
-    <p class="name">{{ userData.name }}さんようこそ</p>
-    <p class="Wallet">残高:{{ userData.wallet }}</p>
+    <p class="name">{{ myData.name }}さんようこそ</p>
+    <p class="Wallet">残高:{{ myData.wallet }}</p>
     <button @click="logout()">ログアウト</button>
     <h1>ユーザー覧</h1>
 
@@ -12,46 +12,76 @@
       <tr v-for="user in usersData" :key="user.id">
         <td>{{ user.name }}</td>
         <td><button @click="openWallet(user)">walletを見る</button></td>
-        <td><button>送る</button></td>
+        <td><button @click="openSend(user)">送る</button></td>
       </tr>
     </table>
 
-    <div id="overlay" v-show="showContent">
-      <div id="content">
-        <p>
-          {{ viewData.name }}さんの残高<br /><br />
-          {{ viewData.wallet }}
-        </p>
-        <div>
-          <button @click="closeWallet">close</button>
-        </div>
-      </div>
-    </div>
+    <UserWallet :user="user" v-show="viewContent" v-on:close-click="closeWallet"></UserWallet>
+    <SendWallet v-on:wallet-click="wallet = $event" v-on:send-click="sendWallet" :myData="myData" v-show="sendContent"></SendWallet>
   </div>
 </template>
 
-//
+
 <script>
 import firebase from 'firebase';
+import UserWallet from '../components/UserWallet.vue';
+import SendWallet from '../components/SendWallet.vue';
 
 export default {
+  components: {
+    UserWallet,
+    SendWallet,
+  },
   data() {
     return {
-      userData: {},
+      myData: {},
       usersData: [],
-      viewData: {},
-      showContent: false,
+      user: {},
+      viewContent: false,
+      sendContent: false,
+      wallet: '',
     };
   },
 
   methods: {
     openWallet(user) {
-      this.viewData = user;
-      this.showContent = true;
+      this.user = user;
+      this.viewContent = true;
     },
     closeWallet() {
-      this.showContent = false;
+      this.viewContent = false;
     },
+    openSend(user) {
+      this.user = user;
+      this.sendContent = true;
+    },
+    sendWallet() {
+      if (this.myData.wallet >= this.wallet) {
+        this.fb(this.myData.uid)
+          .update({
+            wallet: this.myData.wallet - this.wallet,
+          })
+          .then(() => {
+            this.fb(this.user.uid)
+              .update({
+                wallet: this.user.wallet + this.wallet,
+              })
+              .then(() => {
+                this.usersData = [];
+                this.reload();
+                this.sendContent = false;
+              });
+          });
+      }
+    },
+
+    fb(uid) {
+      return firebase
+        .firestore()
+        .collection('users')
+        .doc(uid);
+    },
+
     logout() {
       firebase
         .auth()
@@ -60,41 +90,45 @@ export default {
           this.$router.push('/');
         });
     },
+
+    reload() {
+      if (firebase.auth().currentUser == null) {
+        this.$router.push('/');
+      } else {
+        firebase
+          .firestore()
+          .collection('users')
+          .doc(firebase.auth().currentUser.uid)
+          .get()
+          .then((doc) => {
+            this.myData = doc.data();
+            firebase
+              .firestore()
+              .collection('users')
+              .get()
+              .then((docs) => {
+                docs.forEach((user) => {
+                  if (firebase.auth().currentUser.uid == user.data().uid) {
+                    return;
+                  } else {
+                    this.usersData.push(user.data());
+                  }
+                });
+              });
+          })
+          .catch(() => {
+            this.$router.push('/');
+          });
+      }
+    },
   },
   beforeRouterLeave(to, from, next) {
-    this.userData = {};
+    this.myData = {};
     this.usersData = [];
     next();
   },
-  created: function() {
-    if (firebase.auth().currentUser == null) {
-      this.$router.push('/');
-    } else {
-      firebase
-        .firestore()
-        .collection('users')
-        .doc(firebase.auth().currentUser.uid)
-        .get()
-        .then((doc) => {
-          this.userData = doc.data();
-          firebase
-            .firestore()
-            .collection('users')
-            .get()
-            .then((docs) => {
-              docs.forEach((user) => {
-                if (firebase.auth().currentUser.uid == user.data().uid) {
-                  return;
-                } else {
-                  this.usersData.push(user.data());
-                }
-              });
-            });
-        })
-        .catch(() => {
-          this.$router.push('/');
-        });
-    }
+  created() {
+    this.reload();
   },
 };
 </script>
